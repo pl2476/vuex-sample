c4860356019c18b31ba350f5874b3774bc1b5432
Light <pl2476@126.com>
2019-08-15 15:25:39 +0800

    make CustomizeCalendar into SFC

--- src/components/CustomizeCalendar/index.tsx
+++ src/components/CustomizeCalendar/index.tsx
@@ -1,5 +1,5 @@
 import { Calendar, Icon, Col, Row } from 'antd';
-import React, { Component } from 'react';
+import React from 'react';
 import moment, { Moment } from 'moment';
 import style from './index.less';
 
@@ -11,46 +11,44 @@ export interface CustomizeCalendarProps {
   onSelect?: (value: Moment | undefined) => void;
 }
 
-// const CustomizeCalendar: React.SFC<CustomizeCalendarProps> = (props) => {
-export default class HeaderSearch extends Component<CustomizeCalendarProps> {
-  constructor(props: CustomizeCalendarProps) {
-    super(props);
-    this.state = {};
-  }
+// export default class HeaderSearch extends Component<CustomizeCalendarProps> {
+const CustomizeCalendar: React.SFC<CustomizeCalendarProps> = props => (
+  // constructor(props: CustomizeCalendarProps) {
+  //   super(props);
+  //   this.state = {};
+  // }
 
-  render() {
-    const { time, mode, nextMonth, lastMonth, onSelect } = this.props;
-    return (
-      <div style={{ width: 280 }}>
-        <Calendar
-          fullscreen={false}
-          headerRender={() => (
-            <div style={{ padding: 10 }}>
-              <Row type="flex" justify="space-between">
-                <Col
-                  style={{ paddingRight: '6px', paddingLeft: '6px', cursor: 'pointer' }}
-                  onClick={lastMonth}
-                >
-                  <Icon type="double-left" />
-                </Col>
-                <Col>
-                  <span>{moment(time).format('MMMM YYYY')}</span>
-                </Col>
-                <Col
-                  style={{ paddingRight: '6px', paddingLeft: '6px', cursor: 'pointer' }}
-                  onClick={nextMonth}
-                >
-                  <Icon type="double-right" />
-                </Col>
-              </Row>
-            </div>
-          )}
-          onSelect={onSelect}
-          value={moment(time)}
-          mode={mode}
-          className={style['customize-calendar']}
-        />
-      </div>
-    );
-  }
-}
+  // const { time, mode, nextMonth, lastMonth, onSelect } = this.props;
+  <div style={{ width: 280 }}>
+    <Calendar
+      fullscreen={false}
+      headerRender={() => (
+        <div style={{ padding: 10 }}>
+          <Row type="flex" justify="space-between">
+            <Col
+              style={{ paddingRight: '6px', paddingLeft: '6px', cursor: 'pointer' }}
+              onClick={props.lastMonth}
+            >
+              <Icon type="double-left" />
+            </Col>
+            <Col>
+              <span>{moment(props.time).format('MMMM YYYY')}</span>
+            </Col>
+            <Col
+              style={{ paddingRight: '6px', paddingLeft: '6px', cursor: 'pointer' }}
+              onClick={props.nextMonth}
+            >
+              <Icon type="double-right" />
+            </Col>
+          </Row>
+        </div>
+      )}
+      onSelect={props.onSelect}
+      value={moment(props.time)}
+      mode={props.mode}
+      className={style['customize-calendar']}
+    />
+  </div>
+);
+
+export default CustomizeCalendar;
