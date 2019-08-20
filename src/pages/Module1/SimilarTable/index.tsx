11016dd2dd50a786514088c92754bdbda4eb8005
Light <pl2476@126.com>
2019-08-20 14:42:57 +0800

    SimilarTable: Update: drag and drop

--- src/pages/Module1/SimilarTable/index.tsx
+++ src/pages/Module1/SimilarTable/index.tsx
@@ -6,9 +6,17 @@ import CustomizeCalendar from '@/components/CustomizeCalendar';
 
 const { Option } = Select;
 
+interface Item {
+  id: number;
+  desc: string;
+  x: number;
+  y: number;
+  h: number;
+}
+
 interface MyState {
   time: number;
-  items: string[];
+  items: Item[];
   isExpand: boolean;
 }
 
@@ -17,7 +25,15 @@ class SimilarTable extends PureComponent<{}, MyState> {
     super(props);
     this.state = {
       time: new Date().getTime(),
-      items: ['a'],
+      items: [
+        {
+          id: 1,
+          desc: 'a',
+          x: 1,
+          y: 2,
+          h: 2,
+        },
+      ],
       isExpand: false,
     };
   }
@@ -67,6 +83,53 @@ class SimilarTable extends PureComponent<{}, MyState> {
     this.setState({ isExpand: !isExpand });
   };
 
+  dragStart = (item: object, event?: React.DragEvent): void => {
+    if (event && event.dataTransfer) {
+      const { dataTransfer } = event;
+      dataTransfer.setData(
+        'text/plain',
+        JSON.stringify({
+          item,
+        }),
+      );
+      dataTransfer.effectAllowed = 'linkMove';
+    }
+  };
+
+  onDrop = (row: number, col: number, e?: React.DragEvent): void => {
+    if (e && e.dataTransfer) {
+      e.dataTransfer.dropEffect = 'move';
+      if (typeof row !== 'number' || col < 1) {
+        e.dataTransfer.dropEffect = 'none';
+        return;
+      }
+      const data = JSON.parse(e.dataTransfer.getData('text/plain')).item;
+      this.setState({
+        items: [
+          {
+            id: data.id,
+            desc: data.desc,
+            x: col,
+            y: row,
+            h: data.h,
+          },
+        ],
+      });
+    }
+  };
+
+  dragEnter = (item: number, e?: React.DragEvent): void => {
+    if (e && e.dataTransfer) {
+      e.preventDefault();
+    }
+  };
+
+  dragOver = (item: number, e?: React.DragEvent): void => {
+    if (e && e.dataTransfer) {
+      e.preventDefault();
+    }
+  };
+
   render() {
     const tableHeadList = [1, 2, 3, 4, 5, 6];
     const tableBodyList = [];
@@ -81,8 +144,14 @@ class SimilarTable extends PureComponent<{}, MyState> {
     ));
     const body = tableBodyList.map((rowItem, i) => (
       <div key={rowItem} className={style.row}>
-        {['', ...tableHeadList].map((colItem, j) => (
-          <div key={colItem} className={style.cell} draggable={j === 0}>
+        {[-1, ...tableHeadList].map((colItem, j) => (
+          <div
+            key={colItem}
+            className={style.cell}
+            onDrop={this.onDrop.bind(this, rowItem, colItem)}
+            onDragEnter={this.dragEnter.bind(this, rowItem)}
+            onDragOver={this.dragOver.bind(this, rowItem)}
+          >
             {j === 0 ? i + 1 : ''}
           </div>
         ))}
@@ -116,7 +185,7 @@ class SimilarTable extends PureComponent<{}, MyState> {
     );
     const contentItems = items.map((item, index) => (
       <Dropdown
-        key={item.toString()}
+        key={item.id}
         overlay={
           <Menu>
             <Menu.Item key="1">Edit</Menu.Item>
@@ -127,21 +196,20 @@ class SimilarTable extends PureComponent<{}, MyState> {
         trigger={['contextMenu', 'click']}
       >
         <div
-          key={item}
+          key={item.id}
           className={`${style.contentItem}  ${index === items.length - 1 ? style.last : ''}`}
           style={{
-            // eslint-disable-next-line no-bitwise
             backgroundColor: '#FF9A74',
             opacity: 0.8,
-            height: '30px',
-            lineHeight: '30px',
-            top: 60,
-            left: 88,
+            height: `${item.h * 30}px`,
+            lineHeight: `${item.h * 30}px`,
+            top: `${(item.y + 1) * 30}`,
+            left: `${(item.x - 1) * 150 + 88}`,
           }}
           draggable
-          // onDragStart={this.dragStart.bind(this, item)}
+          onDragStart={this.dragStart.bind(this, item)}
         >
-          {item}
+          {item.id}
         </div>
       </Dropdown>
     ));
