1b3123a453fed76384bd8769580ac7e094de6951
Light <pl2476@126.com>
2019-09-18 18:07:31 +0800

    Update request.ts

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -77,8 +77,12 @@ request.interceptors.request.use((url, options) => {
 // response拦截器, 处理response
 request.interceptors.response.use((response, options) => {
   const token = response.headers.get('authorization');
+  const { data } = options;
   if (token) {
     localStorage.setItem('auth', token);
+  }
+  if (data && data.isLogout) {
+    localStorage.removeItem('auth');
   }
   return response;
 });
