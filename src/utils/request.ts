7cf18f8b51dee38c6329a545b1b4f60b30c76a80
Light <pl2476@126.com>
2019-10-11 10:09:55 +0800

    Update request.ts

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -31,10 +31,10 @@ const errorHandler = (error: { response: Response }): Response => {
   const { response } = error;
   if (response && response.status) {
     const errorText = codeMessage[response.status] || response.statusText;
-    const { status, url } = response;
+    const { status } = response;
 
     notification.error({
-      message: `请求错误 ${status}: ${url}`,
+      message: `请求错误 ${status}`,
       description: errorText,
     });
     if (status === 401) {
