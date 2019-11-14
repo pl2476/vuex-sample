c75c84cfe319a11d0cb743a1c78de453e3a43be1
Light <pl2476@126.com>
2019-11-14 15:54:07 +0800

    eslint use extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
-const { strictEslint } = require('@umijs/fabric');
-
 module.exports = {
-  ...strictEslint,
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
   globals: {
     page: true,
   },
